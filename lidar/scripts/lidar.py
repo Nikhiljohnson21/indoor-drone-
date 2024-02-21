0272f7a94d7da4e5fb72ad7cdc0c666f3f387809
Nikhil K Johnson <139241900+Nikhiljohnson21@users.noreply.github.com>
2024-02-21 20:20:46 +0530

    lidar.py

--- lidar/scripts/lidar.py
+++ lidar/scripts/lidar.py
@@ -3,20 +3,15 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def scan_callback(data):
-    # Find the index of the minimum distance in the scan data
     min_distance_index = data.ranges.index(min(data.ranges))
     
-    # Calculate the angle associated with the minimum distance
-    angle_min = data.angle_min  # Starting angle of the scan
-    angle_increment = data.angle_increment  # Angular resolution
+    angle_min = data.angle_min  
+    angle_increment = data.angle_increment  
     angle_of_min_distance = angle_min + min_distance_index * angle_increment
 
-    # Define a threshold distance for obstacle detection (adjust as needed)
-    threshold_distance = 0.5  # You can adjust this distance
+    threshold_distance = 0.5  
 
-    # Check if the minimum distance is less than the threshold
     if data.ranges[min_distance_index] < threshold_distance:
-        # An obstacle is detected, print a message
         rospy.loginfo("Obstacle detected!")
 
 if __name__ == '__main__':
